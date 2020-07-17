81a904fedf625009b71278d7d6c318b0029f3f0f
Aahad Aazar <aahad@sudofy.com>
2020-07-17 23:23:44 +0500

    Updates

---
+++ precache-manifest.264837264a2941696d534442486eb12c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfad583cd16ae1b23c2e40c7b444ca8c",
+    "url": "/classic-portfolio/index.html"
+  },
+  {
+    "revision": "50dc930deff6e6f149b2",
+    "url": "/classic-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a5e7eaa8444c8c98e73",
+    "url": "/classic-portfolio/static/js/2.ded79a07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/classic-portfolio/static/js/2.ded79a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50dc930deff6e6f149b2",
+    "url": "/classic-portfolio/static/js/main.21afdaf0.chunk.js"
+  },
+  {
+    "revision": "11a8f5e3fb1e00524bfb",
+    "url": "/classic-portfolio/static/js/runtime-main.64ff789c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/classic-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
